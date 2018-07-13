1b8a4ac42a929a659d078093a28df1254ce1a242
JobGetabu <getabujob@gmail.com>
2018-07-14 01:35:34 +0300

    init views

--- app/src/main/java/com/job/countrycabinrentaltabletapp/MainActivity.java
+++ app/src/main/java/com/job/countrycabinrentaltabletapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.job.countrycabinrentaltabletapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init
+        ImageView imageView = findViewById(R.id.ca_image);
+        TextView desc = findViewById(R.id.ca_description);
+        TextView dates = findViewById(R.id.ca_dates);
+        RadioGroup radioGroup = findViewById(R.id.ca_radio);
+        RadioButton ca1 = findViewById(R.id.ca_radio1);
+        RadioButton ca2 = findViewById(R.id.ca_radio2);
+        Button button = findViewById(R.id.btn);
+
+
+
     }
 }
